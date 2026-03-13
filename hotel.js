e115fb2a7cbd4dbb5016d0afc4f29a3470918b0a
Karl <1471600783@k12.hi.us>
2026-03-12 18:49:07 -1000

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,46 @@
+function datetime() {
+    document.getElementById('placeholder2').innerHTML = new Date()
+}
+
+function lock() {
+   
+    var button = document.getElementById('list')
+  
+    button.disabled = true; 
+    document.getElementById("placeholder3").innerHTML= "Room is locked."
+    document.getElementById('placeholder4').innerHTML = ""
+    
+}
+function unlock() {
+    var button = document.getElementById('list');
+    button.disabled = false
+    document.getElementById("placeholder3").innerHTML = "Room is unlocked."
+    document.getElementById("placeholder4").innerHTML = ""
+}
+    if(items.length >= 5) {
+        button.disabled = true
+        document.getElementById('placeholder3').innerHTML = ""
+        document.getElementById('placeholder4').innerHTML = "Room cannot be unlocked due to being full."
+    
+}
+function clearall() {
+    document.getElementById("placeholder").innerHTML = ""
+    document.getElementById("list").value = ""
+    items = []
+    unlock()
+    document.getElementById("placeholder3").innerHTML = ""
+    document.getElementById("placeholder4").innerHTML = ""
+}
+function remove() {
+   let ritem = document.getElementById('rem').value
+   if(ritem in list) {
+    let reitem = document.getElementsByTagName("ritem")[list]
+    document.getElementById(reitem).value = ""
+    document.getElementById("placeholder").innerHTML = ""
+    document.getElementById("rem").innerHTML = ""
+   }
+   else {
+    document.getElementById('placeholder6').innerHTML = "Guest not found in Guest list. Please enter a valid guest to remove."
+    document.getElementById('rem').value = ""
+   }
+}
